d78e6ec941e97fc6765d7425e470d2a7f80a2663
b14ck-ping <aloha.g133@gmail.com>
2025-11-30 10:55:44 +0300

    ...

--- include/sfml_events_handler.hpp
+++ include/sfml_events_handler.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "types.hpp"
 #include <SFML/Graphics.hpp>
 #include <stdexec/execution.hpp>
 
-#include "types.hpp"
-
 class SfmlEventHandler {
 public:
+    using sender_concept = stdexec::sender_t;
+
+    using completion_signatures = stdexec::completion_signatures<  //
+        stdexec::set_value_t(),                                    //
+        stdexec::set_error_t(std::exception_ptr),                  //
+        stdexec::set_stopped_t()                                   //
+        >;
 
     template <typename Receiver>
     struct OperationState {
@@ -24,16 +30,52 @@ public:
             : receiver_{std::forward<R>(r)}, window_{window}, render_settings_{render_settings}, state_{state},
               zoom_clock_{zoom_clock} {}
 
-        
-        /* Ваш код здесь  */
+        friend void tag_invoke(stdexec::start_t, OperationState &self) noexcept {
+            try {
+                self.HandleEvents();
+                self.HandleContinuousZoom();
+                stdexec::set_value(std::move(self.receiver_));
+            } catch (...) {
+                stdexec::set_error(std::move(self.receiver_), std::current_exception());
+            }
+        }
 
     private:
         void HandleEvents() {
             sf::Event event;
             while (window_.pollEvent(event)) {
                 switch (event.type) {
+                case sf::Event::Closed:
+                    state_.should_exit = true;
+                    break;
 
-                /* Ваш код здесь  */
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        state_.should_exit = true;
+                    }
+                    break;
+
+                case sf::Event::MouseButtonPressed:
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        state_.left_mouse_pressed = true;
+                    } else if (event.mouseButton.button == sf::Mouse::Right) {
+                        state_.right_mouse_pressed = true;
+                    }
+                    break;
+
+                case sf::Event::MouseButtonReleased:
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        state_.left_mouse_pressed = false;
+                    } else if (event.mouseButton.button == sf::Mouse::Right) {
+                        state_.right_mouse_pressed = false;
+                    }
+                    break;
+
+                case sf::Event::Resized:
+                    render_settings_.width = event.size.width;
+                    render_settings_.height = event.size.height;
+                    state_.need_rerender = true;
+                    break;
 
                 default:
                     break;
@@ -66,17 +108,26 @@ public:
             const double new_width = state_.viewport.width() * zoom_factor;
             const double new_height = state_.viewport.height() * zoom_factor;
 
-            /* Ваш код обновления state_ здесь  */
+            state_.viewport.x_min = target_x - (target_x - state_.viewport.x_min) * zoom_factor;
+            state_.viewport.x_max = state_.viewport.x_min + new_width;
+            state_.viewport.y_min = target_y - (target_y - state_.viewport.y_min) * zoom_factor;
+            state_.viewport.y_max = state_.viewport.y_min + new_height;
+
+            state_.need_rerender = true;
         }
     };
 
     SfmlEventHandler(sf::RenderWindow &window, RenderSettings render_settings, AppState &state, sf::Clock &zoom_clock)
         : window_{window}, render_settings_{render_settings}, state_{state}, zoom_clock_{zoom_clock} {}
 
-    /* Ваш код здесь  */
+    template <stdexec::receiver Receiver>
+    friend auto tag_invoke(stdexec::connect_t, SfmlEventHandler &&self, Receiver &&receiver)
+        -> OperationState<std::decay_t<Receiver>> {
+        return OperationState<std::decay_t<Receiver>>{std::forward<Receiver>(receiver), self.window_,
+                                                      self.render_settings_, self.state_, self.zoom_clock_};
+    }
 
 private:
-
     sf::RenderWindow &window_;
     RenderSettings render_settings_;
     AppState &state_;
--- include/sfml_renderer.hpp
+++ include/sfml_renderer.hpp
@@ -8,6 +8,14 @@
 
 class SFMLRender {
 public:
+    using sender_concept = stdexec::sender_t;
+
+    using completion_signatures = stdexec::completion_signatures<  //
+        stdexec::set_value_t(),                                    //
+        stdexec::set_error_t(std::exception_ptr),                  //
+        stdexec::set_stopped_t()                                   //
+        >;
+
     template <typename Receiver>
     struct OperationState {
         Receiver receiver_;
@@ -18,7 +26,61 @@ public:
         sf::RenderWindow &window_;
         RenderSettings render_settings_;
 
-        /* Ваш код здесь */
+        template <typename R>
+        OperationState(R &&rec, RenderResult res, sf::Image &img, sf::Texture &tex, sf::Sprite &spr,
+                       sf::RenderWindow &win, RenderSettings settings)
+            : receiver_(std::forward<R>(rec)), render_result_(std::move(res)), image_{img}, texture_{tex}, sprite_{spr},
+              window_{win}, render_settings_{settings} {}
+
+        friend void tag_invoke(stdexec::start_t, OperationState &self) noexcept {
+            try {
+                if (self.render_result_.color_data.empty()) {
+                    stdexec::set_value(std::move(self.receiver_));
+                    return;
+                }
+                std::size_t height = self.render_result_.color_data.size();
+                std::size_t width = height > 0 ? self.render_result_.color_data[0].size() : 0;
+
+                if (width == 0 || height == 0) {
+                    stdexec::set_error(std::move(self.receiver_), std::make_error_code(std::errc::invalid_argument));
+                    return;
+                }
+
+                self.image_.create(width, height, sf::Color::Black);
+
+                for (std::size_t y = 0; y < height; ++y) {
+                    if (y >= self.render_result_.color_data.size()) {
+                        break;
+                    }
+
+                    for (std::size_t x = 0; x < width; ++x) {
+                        if (x >= self.render_result_.color_data[y].size()) {
+                            break;
+                        }
+
+                        const auto &rgb_color = self.render_result_.color_data[y][x];
+                        sf::Color sf_color(rgb_color.r, rgb_color.g, rgb_color.b);
+                        self.image_.setPixel(x, y, sf_color);
+                    }
+                }
+
+                if (!self.texture_.loadFromImage(self.image_)) {
+                    throw std::runtime_error("Failed to load texture from image");
+                }
+
+                self.sprite_.setTexture(self.texture_, true);
+
+                self.window_.clear();
+                self.window_.draw(self.sprite_);
+                self.window_.display();
+
+                stdexec::set_value(std::move(self.receiver_));
+            } catch (...) {
+                stdexec::set_error(std::move(self.receiver_), std::current_exception());
+            }
+        }
+
+    private:
     };
 
     RenderResult render_result_;
@@ -32,4 +94,16 @@ public:
                sf::RenderWindow &window, RenderSettings render_settings)
         : render_result_(render_result), image_{image}, texture_{texture}, sprite_{sprite}, window_{window},
           render_settings_{render_settings} {}
+
+    template <stdexec::receiver Receiver>
+    friend auto tag_invoke(stdexec::connect_t, SFMLRender &&self, Receiver &&receiver)
+        -> OperationState<std::decay_t<Receiver>> {
+        return OperationState<std::decay_t<Receiver>>{std::forward<Receiver>(receiver),
+                                                      std::move(self.render_result_),
+                                                      self.image_,
+                                                      self.texture_,
+                                                      self.sprite_,
+                                                      self.window_,
+                                                      self.render_settings_};
+    }
 };
